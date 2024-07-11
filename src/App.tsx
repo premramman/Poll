ae49e9252df8c5108a5e850331fcb670adbe94d4
premramman <premramman@gmail.com>
2024-07-11 14:00:02 +0530

    Initialized React project with Vite and MUI

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function ButtonUsage() {
+  return <Button variant="contained">Hello world</Button>;
+}
+
+function App() {
+
+  return (
+    <>
+      Hello World
+      <ButtonUsage></ButtonUsage>
+    </>
+  )
+}
+
+export default App
+
+
