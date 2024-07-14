b7664b2b91164392c3a7d03378f1851e3f4e57c1
premramman <premramman@gmail.com>
2024-07-14 21:44:26 +0530

    Improv: Removed unwanted styles

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, TextField } from '@mui/material';
-import { formButtonBox, signInBtn, createAccountBtn } from './styles';
+import { formButtonBox} from './styles';
 import GLOBALS from '../../data/constants/globals';
 import FormLayout from '../../layouts/FormLayout';
 import FormTitle from '../FormTitle';
@@ -18,8 +18,8 @@ function SignUpForm() {
             </RelatedFieldLayout>
 
             <Box sx={formButtonBox}>
-                <Button size="large" sx={signInBtn}>Already a member? Sign in</Button>
-                <Button variant="contained" size="large" sx={createAccountBtn}>Create</Button>
+                <Button size="large">Already a member? Sign in</Button>
+                <Button variant="contained" size="large">Create</Button>
             </Box>
 
         </FormLayout>
--- src/components/SignUpForm/styles.ts
+++ src/components/SignUpForm/styles.ts
@@ -4,10 +4,3 @@ export const formButtonBox = {
     justifyContent: "flex-end",
     gap: 1
 }
-
-export const createAccountBtn = {
-    // width: "40%"
-}
-
-export const signInBtn = {
-}
